fc6ec7ad85594d383414cf56cb6f14104c7e8a33
Eric Stein <estein27@gmail.com>
2012-05-06 12:45:00 -0400

    The open maps api is picky. I kept getting server errors for missing addresses. A missing address now raises a AddressNotFound error and displays a friendly message.

--- location/geocode.py
+++ location/geocode.py
@@ -1,11 +1,20 @@
 import requests
 import json
 
+class AddressNotFound(Exception):
+    def __init__(self, value):
+        self.parameter = value
+    def __str__(self):
+        return repr(self.parameter)
+
 def geocode(address):
     params = dict(format='json', q=address, email='eric@ericstein.org', limit=1)
     base_url = 'http://nominatim.openstreetmap.org/search'
     r = requests.request('GET', url=base_url, params=params)
     data = r.text
-    parsed = json.loads(data)[0]
+    try:
+        parsed = json.loads(data)[0]
+    except IndexError:
+        raise AddressNotFound("Address not found.")
     return (parsed['display_name'], (parsed['lat'], parsed['lon']),)
 
